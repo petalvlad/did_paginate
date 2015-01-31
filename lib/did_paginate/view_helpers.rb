1304bf23143fe0726094f0944dc7270ece7c45a5
Alex Petropavlovsky <petalvlad@gmail.com>
2015-01-31 16:50:51 +0700

    Render nothing if there is only one page

--- lib/did_paginate/view_helpers.rb
+++ lib/did_paginate/view_helpers.rb
@@ -2,7 +2,7 @@ module DidPaginate
   module ViewHelpers
 
     def did_paginate(template, current_page, total_pages, params, url_builder, options = {})
-      DidPaginate::Pager.new(current_page, total_pages, params, url_builder, options).render(template)    
+      DidPaginate::Pager.new(current_page, total_pages, params, url_builder, options).render(template) if total_pages > 1
     end
 
   end
